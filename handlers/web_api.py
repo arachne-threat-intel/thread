2af0b6ba1a9f16f05f5d58a7e73252190935b4e7
jecarr <jenine_c@outlook.com>
2021-08-05 14:36:54 +1200

    Replaced server in resp headers in web_api.py

--- handlers/web_api.py
+++ handlers/web_api.py
@@ -1,7 +1,14 @@
 import json
 
-from aiohttp_jinja2 import template, web
+from aiohttp_jinja2 import render_template, web
 from urllib.parse import quote
+
+# The different types of templates Tram is using
+ABOUT, INDEX, EDIT = 'about', 'index', 'edit'
+TEMPLATES = {ABOUT: 'about.html', INDEX: 'index.html', EDIT: 'columns.html'}
+# Obfuscate server in response headers
+SERVER = 'Server'
+SERVER_VAL = 'Squeak, squeakin\', squeakity'
 
 
 def sanitise_filename(filename=''):
@@ -12,8 +19,16 @@ def sanitise_filename(filename=''):
     return ''.join([x if (x.isalnum() or x in '-\'') else '_' for x in temp_fn])
 
 
-class WebAPI:
+class WebReqHandler(web.View):
+    async def get(self, html: str, data=None):
+        """Function to render a given template (html) optionally with data."""
+        response = render_template(html, self.request, data)
+        # Can't delete the Server header so leave as blank or junk
+        response.headers[SERVER] = SERVER_VAL
+        return response
 
+
+class WebAPI:
     def __init__(self, services):
         self.dao = services.get('dao')
         self.data_svc = services['data_svc']
@@ -23,11 +38,22 @@ class WebAPI:
         self.rest_svc = services['rest_svc']
         self.report_statuses = self.rest_svc.get_status_enum()
 
-    @template('about.html')
-    async def about(self, request):
-        return
+    @staticmethod
+    def respond_error(message=None):
+        """Function to produce an error JSON response."""
+        if message is None:
+            return web.json_response(None, status=500, headers={SERVER: SERVER_VAL})
+        else:
+            return web.json_response(text=message, status=500, headers={SERVER: SERVER_VAL})
 
-    @template('index.html')
+    @staticmethod
+    def respond_data(data=None, status=200):
+        """Function to produce a JSON response with data."""
+        return web.json_response(data, status=status, headers={SERVER: SERVER_VAL})
+
+    async def about(self, request):
+        return await WebReqHandler(request=request).get(html=TEMPLATES[ABOUT])
+
     async def index(self, request):
         page_data = dict()
         # For each report status, get the reports for the index page
@@ -54,7 +80,7 @@ class WebAPI:
             # Else proceed to obtain the reports for this status as normal
             else:
                 page_data[status.value]['reports'] = await self.data_svc.status_grouper(status.value)
-        return dict(reports_by_status=page_data)
+        return await WebReqHandler(request=request).get(html=TEMPLATES[INDEX], data=dict(reports_by_status=page_data))
 
     async def rest_api(self, request):
         """
@@ -79,7 +105,7 @@ class WebAPI:
                 ))
             method = options[request.method][index]
         except KeyError:
-            return web.json_response(None, status=500)
+            return self.respond_error()
         output = await method(data)
         status = 200
         if output is not None and type(output) != dict:
@@ -90,9 +116,8 @@ class WebAPI:
             status = 202
         elif output.get('error'):
             status = 500
-        return web.json_response(output, status=status)
+        return self.respond_data(data=output, status=status)
 
-    @template('columns.html')
     async def edit(self, request):
         """
         Function to edit report
@@ -107,18 +132,19 @@ class WebAPI:
             # Ensure a valid report title has been passed in the request
             report_id = report[0]['uid']
         except (KeyError, IndexError):
-            return web.json_response(text='Invalid URL', status=500)
+            return self.respond_error(message='Invalid URL')
         # A queued report would pass the above check but be blank; raise an error instead
         if report[0]['current_status'] == self.report_statuses.QUEUE.value:
-            return web.json_response(text='Invalid URL', status=500)
+            return self.respond_error(message='Invalid URL')
         # Proceed to gather the data for the template
         sentences = await self.data_svc.get_report_sentences(report_id)
         attack_uids = await self.data_svc.get_techniques()
         original_html = await self.dao.get('original_html', dict(report_uid=report_id))
         final_html = await self.web_svc.build_final_html(original_html, sentences)
-        return dict(file=request.match_info.get('file'), title=report[0]['title'], title_quoted=title_quoted,
-                    sentences=sentences, attack_uids=attack_uids, original_html=original_html, final_html=final_html,
-                    completed=int(report[0]['current_status'] == self.report_statuses.COMPLETED.value))
+        page_data = dict(file=report_title, title=report[0]['title'], title_quoted=title_quoted, final_html=final_html,
+                         sentences=sentences, attack_uids=attack_uids, original_html=original_html,
+                         completed=int(report[0]['current_status'] == self.report_statuses.COMPLETED.value))
+        return await WebReqHandler(request=request).get(html=TEMPLATES[EDIT], data=page_data)
 
     async def nav_export(self, request):
         """
@@ -133,10 +159,10 @@ class WebAPI:
             # Ensure a valid report title has been passed in the request
             report_id, report_status = report[0]['uid'], report[0]['current_status']
         except (KeyError, IndexError):
-            return web.json_response(None, status=500)
+            return self.respond_error()
         # A queued report would pass the above check but be blank; raise an error instead
         if report_status == self.report_statuses.QUEUE.value:
-            return web.json_response(None, status=500)
+            return self.respond_error()
 
         # Create the layer name and description
         enterprise_layer_description = f"Enterprise techniques used by '{report_title}', ATT&CK"
@@ -162,7 +188,7 @@ class WebAPI:
             
         # Return the layer JSON in the response
         layer = json.dumps(enterprise_layer)
-        return web.json_response(layer)
+        return self.respond_data(data=layer)
 
     async def pdf_export(self, request):
         """
@@ -177,10 +203,10 @@ class WebAPI:
             # Ensure a valid report title has been passed in the request
             report_id, report_status, report_url = report[0]['uid'], report[0]['current_status'], report[0]['url']
         except (KeyError, IndexError):
-            return web.json_response(None, status=500)
+            return self.respond_error()
         # A queued report would pass the above check but be blank; raise an error instead
         if report_status == self.report_statuses.QUEUE.value:
-            return web.json_response(None, status=500)
+            return self.respond_error()
         # Continue with the method and retrieve the report's sentences
         sentences = await self.data_svc.get_report_sentences_with_attacks(report_id=report_id)
 
@@ -216,7 +242,7 @@ class WebAPI:
 
         # Append table to the end
         dd['content'].append({'table': table})
-        return web.json_response(dd)
+        return self.respond_data(data=dd)
 
     async def rebuild_ml(self, request):
         """
