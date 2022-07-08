aabb0d2222142f4bf765abb4577dd72b84b260d1
jecarr <jenine_c@outlook.com>
2022-07-08 18:44:10 +0100

    Added report start/end dates

--- threadcomponents/handlers/web_api.py
+++ threadcomponents/handlers/web_api.py
@@ -271,6 +271,12 @@ class WebAPI:
             sentences=sentences, attack_uids=self.attack_dropdown_list, original_html=original_html, pdf_link=pdf_link,
             nav_link=nav_link, completed=int(report_status == self.report_statuses.COMPLETED.value), help_text=help_text
         )
+        # Prepare the date fields to be interpreted by the front-end
+        for report_date in ['start_date', 'end_date']:
+            saved_date = report[0].get(report_date)
+            if isinstance(saved_date, datetime):  # don't pass datetime objects; convert to string
+                saved_date = saved_date.strftime('%Y-%m-%d')
+            template_data[report_date] = saved_date
         return template_data
 
     async def nav_export(self, request):
