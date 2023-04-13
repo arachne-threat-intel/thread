b111796048d7d184004a36a753f552f160c06d52
jecarr <jenine_c@outlook.com>
2023-04-13 13:34:45 +0100

    Submit report refactor (server-side)

--- threadcomponents/service/rest_svc.py
+++ threadcomponents/service/rest_svc.py
@@ -572,7 +572,7 @@ class RestService:
             request_data[key]
         except (KeyError, TypeError):
             return dict(error='Error inserting report(s).')
-        
+
         # If automatically generated, check the token from the request_data is valid
         if request_data.get('automatically_generated'):
             arachne_token = request_data.get('token')
@@ -580,25 +580,26 @@ class RestService:
                 if not await self.web_svc.arachne_token_is_valid(request=request, token=arachne_token):
                     return dict(error='Report(s) not submitted. Arachne token not valid.')
             else:
-                request_data.update(token=None)
+                return dict(error='Report(s) not submitted. Token expected.')
             return None
-        
+
         # If running locally, there are no further checks but ensure the token is None (to avoid mix of ''s and Nones)
         if self.is_local:
             request_data.update(token=None)
             return None
-        
-        # If not running locally, check the token from the request_data is valid if one is given
-        token = request_data.get('token')  # obtain the token
-        if token:
-            # If there is a token, check it is a valid token
-            user = await self.web_svc.get_username_from_token(request, token=token)
-            # Alert user if they tried to associate their submission with an invalid token
-            if user is None:
-                return dict(error='Report(s) not submitted. Please enter a valid token or confirm you are making this '
-                                  'submission public. To find your token, please refer to your Arachne profile. If '
-                                  'this error is persistent, please contact us.', alert_user=1)
-        # If there is no token, blank data with None (to avoid mix of empty strings and Nones)
+
+        # If not running locally, check if the user is requesting a private submission
+        is_private = request_data.get('private')
+        if is_private:
+            # Obtain the token to make this submission private
+            username, token = await self.web_svc.get_current_arachne_user(request)
+            # Update report-data with token or alert user if this was not possible
+            if token:
+                request_data.update(token=token)
+            else:
+                return dict(error='Report(s) not submitted. Please ensure you are logged into Arachne for this '
+                                  'to be private. If this error is persistent, please contact us.', alert_user=1)
+        # If public (no token), blank data with None (to avoid mix of empty strings and Nones)
         else:
             request_data.update(token=None)
         return None
