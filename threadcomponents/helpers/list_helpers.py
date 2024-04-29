e656bc1a016f908b53406393fb717e38b4d2075b
jecarr <52256544+jecarr@users.noreply.github.com>
2024-04-29 14:01:49 +0100

    Renamed list-variable in list_helpers.py

--- threadcomponents/helpers/list_helpers.py
+++ threadcomponents/helpers/list_helpers.py
@@ -1,5 +1,5 @@
 # Copied from here: https://stackoverflow.com/a/480227
-def dedup_list(list):
+def dedup_list(list_to_dedupe):
     seen = set()
     seen_add = seen.add
-    return [x for x in list if not (x in seen or seen_add(x))]
+    return [x for x in list_to_dedupe if not (x in seen or seen_add(x))]
