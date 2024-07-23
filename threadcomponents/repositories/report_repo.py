7056417867fbc7a3e118853b31e5eef30d757426
jecarr <jenine_c@outlook.com>
2024-07-23 18:35:43 +0100

    Prevented duplicate sentence-mappings being saved

--- threadcomponents/repositories/report_repo.py
+++ threadcomponents/repositories/report_repo.py
@@ -49,17 +49,22 @@ class ReportRepository:
                 found_status=self.dao.db_true_val,
             ),
         )
+
+        saved_tids = set()
         for technique_tid, technique_name in sentence["ml_techniques_found"]:
             attack_uid = await self.dao.get("attack_uids", dict(tid=technique_tid))
+
             # If the attack cannot be found via the 'tid' column, try the 'name' column
             if not attack_uid:
                 attack_uid = await self.dao.get("attack_uids", dict(name=technique_name))
+
             # If the attack has still not been retrieved, try searching the similar_words table
             if not attack_uid:
                 similar_word = await self.dao.get("similar_words", dict(similar_word=technique_name))
                 # If a similar word was found, use its attack_uid to lookup the attack_uids table
                 if similar_word and similar_word[0] and similar_word[0]["attack_uid"]:
                     attack_uid = await self.dao.get("attack_uids", dict(uid=similar_word[0]["attack_uid"]))
+
             # If the attack has still not been retrieved, report to user that this cannot be saved against the sentence
             if not attack_uid:
                 logging.warning(
@@ -77,9 +82,14 @@ class ReportRepository:
                 )
                 # Skip this technique and continue with the next one
                 continue
+
             attack_technique = attack_uid[0]["uid"]
             attack_tech_name = attack_uid[0]["name"]
             attack_tid = attack_uid[0]["tid"]
+
+            if attack_tid in saved_tids:
+                continue
+
             # Allow 'inactive' attacks to be recorded: they will be filtered out when viewing/exporting a report
             data = dict(
                 sentence_id=sentence_id,
@@ -89,6 +99,9 @@ class ReportRepository:
                 attack_tid=attack_tid,
                 initial_model_match=self.dao.db_true_val,
             )
+
             if tech_start_date:
                 data.update(dict(start_date=tech_start_date))
+
             await self.dao.insert_with_backup("report_sentence_hits", data)
+            saved_tids.add(attack_tid)
